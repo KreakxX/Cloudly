7b3686dc50a8467d9125d2c7d63d27a53cfad2dc
KreakxX <Henrik.standke@web.de>
2025-11-13 21:07:23 +0100

    basic structure doe

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/upload", uploadFile)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+
+	var file = r.Body
+
+	if file == nil {
+		return
+	}
+
+	db, err := sql.Open("mysql", "root:1234@cloudly")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.Query("SELECT * FROM cloudly")
+
 }
