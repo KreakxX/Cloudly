30bc48753458bf4f8fc5a1ad24c890637e5d455c
KreakxX <Henrik.standke@web.de>
2025-11-14 10:17:45 +0100

    updated uploadFile with reading and error handling

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"io"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -13,12 +15,26 @@ func main() {
 }
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		http.Error(w, "Error while parsing File, might be too large", http.StatusInternalServerError)
+	}
 
-	var file = r.Body
+	file, header, err := r.FormFile("upload")
 
-	if file == nil {
+	print(header)
+
+	if err != nil {
 		return
 	}
+
+	fileBytes, err := io.ReadAll(file)
+
+	if err != nil {
+		http.Error(w, "Error while reading file", http.StatusInternalServerError)
+	}
+
+	print(fileBytes)
 
 	db, err := sql.Open("mysql", "root:1234@cloudly")
 
@@ -26,6 +42,8 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	db.Query("SELECT * FROM cloudly")
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
 
 }
